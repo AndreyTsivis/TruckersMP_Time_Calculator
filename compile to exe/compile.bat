924c77462a6ba56b84e8f6028ecf704f22f58036
AndreyTsivis <AndreyTsivis19@gmail.com>
2022-11-04 21:31:27 +0300

    modified:   compile to exe/compile.bat

--- compile to exe/compile.bat
+++ compile to exe/compile.bat
@@ -2,9 +2,9 @@ echo off
 
 cd ..
 pip install pyinstaller
-pyinstaller --onefile --distpath "compile to exe/" "main.py"
+pyinstaller --onefile --distpath "compile to exe/" "TruckersMP-Time-Calc.py"
 
-del /s /q /f main.spec
+del /s /q /f TruckersMP-Time-Calc.spec
 rmdir /s /q __pycache__
 rmdir /s /q build
 
