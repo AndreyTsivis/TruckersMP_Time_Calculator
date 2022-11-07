6b185b52f55d7f682296ddfe3578fd8b47adfc55
AndreyTsivis <AndreyTsivis19@gmail.com>
2022-11-07 22:40:12 +0300

    Cringe

--- TruckersMP-Time-Calc.py
+++ TruckersMP-Time-Calc.py
@@ -1,14 +1,29 @@
-from datetime import datetime
+class TimeCalc:
+    def gametime_to_real(self, hours, mins) -> str:
+        from datetime import datetime
 
-game_time_in = input("Input game time in \"hh:mm\" format: ")
-game_time_split = game_time_in.split(":") 
+        game_time_secs = (int(hours)*60 + int(mins))*60
 
-game_time_secs = (int(game_time_split[0])*60 + int(game_time_split[1]))*60
+        real_time_secs = game_time_secs / 10
+        real_time_hours = real_time_secs // 60**2
+        real_time_mins = round(real_time_secs / 60**2 - real_time_hours, 3) * 60
 
-real_time_secs = game_time_secs / 10
-real_time_hours = real_time_secs // 60**2
-real_time_mins = round(real_time_secs / 60**2 - real_time_hours, 3) * 60
+        now = datetime.strptime(f"{round(real_time_hours)}:{round(real_time_mins)}", '%H:%M')
+        return now.strftime('%H:%M')
 
-now = datetime.strptime(f"{round(real_time_hours)}:{round(real_time_mins)}", '%H:%M')
-print(f"Real time: {now.strftime('%H:%M')}")
-input("\n\nPress Enter to exit")
+    def dist_speed_to_realtime(self, dist:int, speed:int) -> str:
+        from datetime import datetime
+
+        game_time_secs = ((round((dist / speed)))*60 + (round(((dist / speed) - round((dist / speed),0)) * 60)))*60
+
+        real_time_secs = game_time_secs / 10
+        real_time_hours = real_time_secs // 60**2
+        real_time_mins = round(real_time_secs / 60**2 - real_time_hours, 3) * 60
+
+        now = datetime.strptime(f"{round(real_time_hours)}:{round(real_time_mins)}", '%H:%M')
+        return now.strftime('%H:%M')
+
+if __name__ == "__main__":
+    dist = int(input("Введите дистацию: "))
+    speed = int(input("Введите скорость: "))
+    print(f"Реальное время: {TimeCalc().dist_speed_to_realtime(dist, speed)}")
